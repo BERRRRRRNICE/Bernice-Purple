1e1cb08d303f7784a51953a796ccd83e221d02e6
Bernice <bernice.huang101@gmail.com>
2023-04-08 14:57:20 -0400

    Breakout

--- BH - Breakout/Assets/Player.cs
+++ BH - Breakout/Assets/Player.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed;
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * speed);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
     }
 }
